e04ade3ab704304a5c7cd2f2d0412a6191eb66df
IshanSinha-26 <ishansinha2611@gmail.com>
2025-05-10 06:42:55 +0000

    datatypes

---
+++ 01_basic/datatype.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+let name="ishan"
+let age= 20
+let loggedIn= false
+
+//number=> 2 to power 53
+//bigint
+//string=""
+//boolean=> true/false
+//undefined=> value is not defined
+//null=> standalone value
+//symbol=> unique
+
+//object
+
+console.log(typeof "false")
+console.log(typeof 18)
+console.log(typeof undefined)
+console.log(typeof null)
