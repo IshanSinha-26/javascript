d0c6ef8f796e4009c82f1cf1e3e07b574624347f
IshanSinha-26 <ishansinha2611@gmail.com>
2025-05-11 08:54:47 +0000

    conversion  of datatype

---
+++ 01_basic/conversionOperation.js
@@ -0,0 +1,21 @@
+let score="33abc"
+console.log(typeof score)
+console.log(typeof(score))
+
+let newScore= Number(score)
+console.log(typeof newScore)
+console.log(newScore)
+
+//"33"=>33
+//"33abc"=>Nan
+//"Null"=>0
+//"Undefined"=>Nan
+//true=1; false=0;
+
+let boolean= ""
+let newBoolean= Boolean(boolean)
+console.log(newBoolean)
+
+//1=>true 0=>false
+//""=>false
+//"ishan"=> true
