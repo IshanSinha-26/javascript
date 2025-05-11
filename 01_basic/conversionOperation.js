86955bdc0b84488f07fc5e1b04792a6a47f8876a
IshanSinha-26 <ishansinha2611@gmail.com>
2025-05-11 20:40:53 +0000

    conversion of datatype

--- 01_basic/conversionOperation.js
+++ 01_basic/conversionOperation.js
@@ -1,10 +1,10 @@
 let score="33abc"
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
 let newScore= Number(score)
-console.log(typeof newScore)
-console.log(newScore)
+// console.log(typeof newScore)
+// console.log(newScore)
 
 //"33"=>33
 //"33abc"=>Nan
@@ -14,8 +14,36 @@ console.log(newScore)
 
 let boolean= ""
 let newBoolean= Boolean(boolean)
-console.log(newBoolean)
+// console.log(newBoolean)
 
 //1=>true 0=>false
 //""=>false
 //"ishan"=> true
+
+// *************************************** Operation **********************************************
+
+let value= 3
+let negValue = -3
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3) 
+
+let str1="hi"
+let str2=" ishan"
+// let str3= str1 + str2
+// console.log(str3)
+
+// console.log(1 + "2")
+// console.log("2" + 1)
+// console.log(1+2+"2")
+// console.log("1"+2+2)
+
+let gameCounter=100
+gameCounter++
+// console.log(gameCounter)
+
