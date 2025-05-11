03deb88c87fa45395d9574951654e638d3a912fb
IshanSinha-26 <ishansinha2611@gmail.com>
2025-05-11 21:04:47 +0000

    comparison

---
+++ 01_basic/comparison.js
@@ -0,0 +1,15 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+
+// console.log("2">1)
+// console.log(2>"1")
+
+// console.log(null>0)
+// console.log(null==0) //comparison and equality check are two different things
+// console.log(null>=0)
+
+console.log("2"===2) //strict check(===)
+
