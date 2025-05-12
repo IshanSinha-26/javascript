bd5db8ad0320dec21b7252b0b4ce045bac9e682e
IshanSinha-26 <ishansinha2611@gmail.com>
2025-05-12 09:03:28 +0000

    strings

---
+++ 01_basic/string.js
@@ -0,0 +1,31 @@
+const name="ishan"
+const age="20"
+// console.log(name + age);
+// console.log(`hello my name is ${name} and my age is ${age}`) //string interpolation
+
+const gameName= new String('dead man')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.fontcolor())
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf("n"))
+// console.log(gameName.substring(0,4))
+
+const anotherString = gameName.slice(-7, 4)
+// console.log(anotherString);
+
+const newStringOne = "   ishan    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://ishan.com/ishan%20sinha"
+
+// console.log(url.replace('%20', '-'))
+
+// console.log(url.includes('sundar'))
+
+// console.log(gameName.split(' '));
+
+
